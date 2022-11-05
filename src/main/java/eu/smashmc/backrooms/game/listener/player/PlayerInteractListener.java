13020744ed5a6fa9d7b6e6c5f95a388caa313f91
ReaperMaga <reapermaga@gmail.com>
2022-11-05 22:20:59 +0100

    Fixed levers multiple task solving

--- src/main/java/eu/smashmc/backrooms/game/listener/player/PlayerInteractListener.java
+++ src/main/java/eu/smashmc/backrooms/game/listener/player/PlayerInteractListener.java
@@ -1,5 +1,6 @@
 package eu.smashmc.backrooms.game.listener.player;
 
+import eu.smashmc.backrooms.BackroomsConstants;
 import eu.smashmc.backrooms.game.GameService;
 import eu.smashmc.backrooms.game.model.Game;
 import eu.smashmc.backrooms.game.model.GameState;
@@ -7,6 +8,7 @@ import eu.smashmc.backrooms.game.participant.GameParticipant;
 import eu.smashmc.backrooms.game.participant.scientist.ScientistParticipant;
 import in.prismar.library.meta.anno.Inject;
 import in.prismar.library.spigot.meta.anno.AutoListener;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.type.Switch;
@@ -16,6 +18,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Copyright (c) Maga, All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
@@ -24,6 +29,8 @@ import org.bukkit.event.player.PlayerInteractEvent;
  **/
 @AutoListener
 public class PlayerInteractListener implements Listener {
+
+    public static final Set<Location> USED_LEVERS = new HashSet<>();
 
     @Inject
     private GameService service;
@@ -44,8 +51,15 @@ public class PlayerInteractListener implements Listener {
                     if(game.getState() != GameState.IN_GAME) {
                         return;
                     }
+                    if(USED_LEVERS.contains(block.getLocation())) {
+                        player.sendMessage(BackroomsConstants.PREFIX + "§7This lever is already activated.");
+                        return;
+                    }
                     GameParticipant participant = game.getParticipantRegistry().getParticipant(player.getUniqueId());
                     if(participant instanceof ScientistParticipant) {
+                        power.setPowered(true);
+                        block.setBlockData(power);
+                        USED_LEVERS.add(block.getLocation());
                         service.solveTask(game);
                     } else {
                         event.setCancelled(true);
